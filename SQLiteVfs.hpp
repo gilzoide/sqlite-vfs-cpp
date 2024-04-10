f371c994f7a00bae363fc8fc0f15afb418c323ba
gilzoide <gilzoide@gmail.com>
2024-04-10 07:12:26 -0300

    Rename "SQLiteVfsShim" -> "SQLiteVfs"

--- SQLiteVfs.hpp
+++ SQLiteVfs.hpp
@@ -1,17 +1,19 @@
-#ifndef __SQLITE_VFS_SHIM_HPP__
-#define __SQLITE_VFS_SHIM_HPP__
+#ifndef __SQLITE_VFS_HPP__
+#define __SQLITE_VFS_HPP__
 
 #include <sqlite3.h>
 
 /**
- * SQLite File shim class with virtual methods for C++.
+ * SQLite File class with virtual methods for C++.
+ *
+ * By default, it forwards every call to the `original_file` passed to it in its constructor.
  *
  * @see https://sqlite.org/c3ref/file.html
  */
-class SQLiteFileShim : sqlite3_file {
+class SQLiteFile : sqlite3_file {
 public:
-	SQLiteFileShim();
-	SQLiteFileShim(sqlite3_file *original_file);
+	SQLiteFile();
+	SQLiteFile(sqlite3_file *original_file);
 
 	virtual int xClose();
 	virtual int xRead(void *p, int iAmt, sqlite3_int64 iOfst);
@@ -43,12 +45,15 @@ protected:
 /**
  * SQLite VFS shim class with virtual methods for C++.
  *
+ * By default, it forwards every call to the `original_vfs` passed to it in its constructor.
+ *
  * @see https://sqlite.org/c3ref/vfs.html
  */
-class SQLiteVfsShim : sqlite3_vfs {
+class SQLiteVfs : sqlite3_vfs {
 public:
-	SQLiteVfsShim(const char *name, const char *base_vfs_name = nullptr);
-	SQLiteVfsShim(const char *name, int file_shim_size, const char *base_vfs_name = nullptr);
+	SQLiteVfs(const char *name, const char *base_vfs_name = nullptr);
+	SQLiteVfs(const char *name, int file_shim_size, const char *base_vfs_name = nullptr);
+	SQLiteVfs(const char *name, int file_shim_size, sqlite3_vfs *original_vfs);
 	
 	int register_vfs(bool makeDefault);
 	int unregister_vfs();
@@ -103,195 +108,200 @@ protected:
 	sqlite3_vfs *original_vfs;
 };
 
-#endif
+#endif  // __SQLITE_VFS_HPP__
 
 ///////////////////////////////////////////////////////////
 // Implementation
 ///////////////////////////////////////////////////////////
 #ifdef SQLITE_VFS_SHIM_IMPLEMENTATION
 
-template<auto SQLiteFileShim::*fptr, typename... Args>
+template<auto SQLiteFile::*fptr, typename... Args>
 static auto wrap_file_method(sqlite3_file *file, Args... args) {
-	return (((SQLiteFileShim *) file)->*fptr)(args...);
+	return (((SQLiteFile *) file)->*fptr)(args...);
 }
 
 static sqlite3_io_methods SQLiteFileShimMethods = {
-  3,
-  wrap_file_method<&SQLiteFileShim::xClose>,
-  wrap_file_method<&SQLiteFileShim::xRead>,
-  wrap_file_method<&SQLiteFileShim::xWrite>,
-  wrap_file_method<&SQLiteFileShim::xTruncate>,
-  wrap_file_method<&SQLiteFileShim::xSync>,
-  wrap_file_method<&SQLiteFileShim::xFileSize>,
-  wrap_file_method<&SQLiteFileShim::xLock>,
-  wrap_file_method<&SQLiteFileShim::xUnlock>,
-  wrap_file_method<&SQLiteFileShim::xCheckReservedLock>,
-  wrap_file_method<&SQLiteFileShim::xFileControl>,
-  wrap_file_method<&SQLiteFileShim::xSectorSize>,
-  wrap_file_method<&SQLiteFileShim::xDeviceCharacteristics>,
-  wrap_file_method<&SQLiteFileShim::xShmMap>,
-  wrap_file_method<&SQLiteFileShim::xShmLock>,
-  wrap_file_method<&SQLiteFileShim::xShmBarrier>,
-  wrap_file_method<&SQLiteFileShim::xShmUnmap>,
-  wrap_file_method<&SQLiteFileShim::xFetch>,
-  wrap_file_method<&SQLiteFileShim::xUnfetch>,
+	3,
+	wrap_file_method<&SQLiteFile::xClose>,
+	wrap_file_method<&SQLiteFile::xRead>,
+	wrap_file_method<&SQLiteFile::xWrite>,
+	wrap_file_method<&SQLiteFile::xTruncate>,
+	wrap_file_method<&SQLiteFile::xSync>,
+	wrap_file_method<&SQLiteFile::xFileSize>,
+	wrap_file_method<&SQLiteFile::xLock>,
+	wrap_file_method<&SQLiteFile::xUnlock>,
+	wrap_file_method<&SQLiteFile::xCheckReservedLock>,
+	wrap_file_method<&SQLiteFile::xFileControl>,
+	wrap_file_method<&SQLiteFile::xSectorSize>,
+	wrap_file_method<&SQLiteFile::xDeviceCharacteristics>,
+	wrap_file_method<&SQLiteFile::xShmMap>,
+	wrap_file_method<&SQLiteFile::xShmLock>,
+	wrap_file_method<&SQLiteFile::xShmBarrier>,
+	wrap_file_method<&SQLiteFile::xShmUnmap>,
+	wrap_file_method<&SQLiteFile::xFetch>,
+	wrap_file_method<&SQLiteFile::xUnfetch>,
 };
 
 // File
-SQLiteFileShim::SQLiteFileShim()
+SQLiteFile::SQLiteFile()
 	: sqlite3_file()
 {
 }
-SQLiteFileShim::SQLiteFileShim(sqlite3_file *original_file)
+SQLiteFile::SQLiteFile(sqlite3_file *original_file)
 	: sqlite3_file({ &SQLiteFileShimMethods }), original_file(original_file)
 {
 }
 
-int SQLiteFileShim::xClose() {
+int SQLiteFile::xClose() {
 	return original_file->pMethods->xClose(original_file);
 }
-int SQLiteFileShim::xRead(void *p, int iAmt, sqlite3_int64 iOfst) {
+int SQLiteFile::xRead(void *p, int iAmt, sqlite3_int64 iOfst) {
 	return original_file->pMethods->xRead(original_file, p, iAmt, iOfst);
 }
-int SQLiteFileShim::xWrite(const void *p, int iAmt, sqlite3_int64 iOfst) {
+int SQLiteFile::xWrite(const void *p, int iAmt, sqlite3_int64 iOfst) {
 	return original_file->pMethods->xWrite(original_file, p, iAmt, iOfst);
 }
-int SQLiteFileShim::xTruncate(sqlite3_int64 size) {
+int SQLiteFile::xTruncate(sqlite3_int64 size) {
 	return original_file->pMethods->xTruncate(original_file, size);
 }
-int SQLiteFileShim::xSync(int flags) {
+int SQLiteFile::xSync(int flags) {
 	return original_file->pMethods->xSync(original_file, flags);
 }
-int SQLiteFileShim::xFileSize(sqlite3_int64 *pSize) {
+int SQLiteFile::xFileSize(sqlite3_int64 *pSize) {
 	return original_file->pMethods->xFileSize(original_file, pSize);
 }
-int SQLiteFileShim::xLock(int flags) {
+int SQLiteFile::xLock(int flags) {
 	return original_file->pMethods->xLock(original_file, flags);
 }
-int SQLiteFileShim::xUnlock(int flags) {
+int SQLiteFile::xUnlock(int flags) {
 	return original_file->pMethods->xUnlock(original_file, flags);
 }
-int SQLiteFileShim::xCheckReservedLock(int *pResOut) {
+int SQLiteFile::xCheckReservedLock(int *pResOut) {
 	return original_file->pMethods->xCheckReservedLock(original_file, pResOut);
 }
-int SQLiteFileShim::xFileControl(int op, void *pArg) {
+int SQLiteFile::xFileControl(int op, void *pArg) {
 	return original_file->pMethods->xFileControl(original_file, op, pArg);
 }
-int SQLiteFileShim::xSectorSize() {
+int SQLiteFile::xSectorSize() {
 	return original_file->pMethods->xSectorSize(original_file);
 }
-int SQLiteFileShim::xDeviceCharacteristics() {
+int SQLiteFile::xDeviceCharacteristics() {
 	return original_file->pMethods->xDeviceCharacteristics(original_file);
 }
-int SQLiteFileShim::xShmMap(int iPg, int pgsz, int flags, void volatile **pp) {
+int SQLiteFile::xShmMap(int iPg, int pgsz, int flags, void volatile **pp) {
 	return original_file->pMethods->xShmMap(original_file, iPg, pgsz, flags, pp);
 }
-int SQLiteFileShim::xShmLock(int offset, int n, int flags) {
+int SQLiteFile::xShmLock(int offset, int n, int flags) {
 	return original_file->pMethods->xShmLock(original_file, offset, n, flags);
 }
-void SQLiteFileShim::xShmBarrier() {
+void SQLiteFile::xShmBarrier() {
 	return original_file->pMethods->xShmBarrier(original_file);
 }
-int SQLiteFileShim::xShmUnmap(int deleteFlag) {
+int SQLiteFile::xShmUnmap(int deleteFlag) {
 	return original_file->pMethods->xShmUnmap(original_file, deleteFlag);
 }
-int SQLiteFileShim::xFetch(sqlite3_int64 iOfst, int iAmt, void **pp) {
+int SQLiteFile::xFetch(sqlite3_int64 iOfst, int iAmt, void **pp) {
 	return original_file->pMethods->xFetch(original_file, iOfst, iAmt, pp);
 }
-int SQLiteFileShim::xUnfetch(sqlite3_int64 iOfst, void *p) {
+int SQLiteFile::xUnfetch(sqlite3_int64 iOfst, void *p) {
 	return original_file->pMethods->xUnfetch(original_file, iOfst, p);
 }
 
 // VFS
-template<auto SQLiteVfsShim::*fptr, typename... Args>
+template<auto SQLiteVfs::*fptr, typename... Args>
 static auto wrap_vfs_method(sqlite3_vfs *vfs, Args... args) {
-	return (((SQLiteVfsShim *) vfs)->*fptr)(args...);
+	return (((SQLiteVfs *) vfs)->*fptr)(args...);
 }
 
-SQLiteVfsShim::SQLiteVfsShim(const char *name, const char *base_vfs_name)
-	: SQLiteVfsShim(name, sizeof(SQLiteFileShim), base_vfs_name)
+SQLiteVfs::SQLiteVfs(const char *name, const char *base_vfs_name)
+	: SQLiteVfs(name, sizeof(SQLiteFile), base_vfs_name)
 {
 }
-SQLiteVfsShim::SQLiteVfsShim(const char *name, int file_shim_size, const char *base_vfs_name) {
-	original_vfs = sqlite3_vfs_find(base_vfs_name) ?: sqlite3_vfs_find(nullptr);
+SQLiteVfs::SQLiteVfs(const char *name, int file_shim_size, const char *base_vfs_name)
+	: SQLiteVfs(name, file_shim_size, sqlite3_vfs_find(base_vfs_name) ?: sqlite3_vfs_find(nullptr))
+{
+}
+SQLiteVfs::SQLiteVfs(const char *name, int file_shim_size, sqlite3_vfs *original_vfs)
+	: original_vfs(original_vfs)
+{
 	iVersion = original_vfs->iVersion;
 	szOsFile = (int) file_shim_size + original_vfs->szOsFile;
 	mxPathname = original_vfs->mxPathname;
 	pNext = nullptr;
 	zName = name;
 	pAppData = nullptr;
-	sqlite3_vfs::xOpen = wrap_vfs_method<&SQLiteVfsShim::xOpen>;
-	sqlite3_vfs::xDelete = wrap_vfs_method<&SQLiteVfsShim::xDelete>;
-	sqlite3_vfs::xAccess = wrap_vfs_method<&SQLiteVfsShim::xAccess>;
-	sqlite3_vfs::xFullPathname = wrap_vfs_method<&SQLiteVfsShim::xFullPathname>;
-	sqlite3_vfs::xDlOpen = wrap_vfs_method<&SQLiteVfsShim::xDlOpen>;
-	sqlite3_vfs::xDlError = wrap_vfs_method<&SQLiteVfsShim::xDlError>;
-	sqlite3_vfs::xDlSym = wrap_vfs_method<&SQLiteVfsShim::xDlSym>;
-	sqlite3_vfs::xDlClose = wrap_vfs_method<&SQLiteVfsShim::xDlClose>;
-	sqlite3_vfs::xRandomness = wrap_vfs_method<&SQLiteVfsShim::xRandomness>;
-	sqlite3_vfs::xSleep = wrap_vfs_method<&SQLiteVfsShim::xSleep>;
-	sqlite3_vfs::xCurrentTime = wrap_vfs_method<&SQLiteVfsShim::xCurrentTime>;
-	sqlite3_vfs::xGetLastError = wrap_vfs_method<&SQLiteVfsShim::xGetLastError>;
-	sqlite3_vfs::xCurrentTimeInt64 = wrap_vfs_method<&SQLiteVfsShim::xCurrentTimeInt64>;
-	sqlite3_vfs::xSetSystemCall = wrap_vfs_method<&SQLiteVfsShim::xSetSystemCall>;
-	sqlite3_vfs::xGetSystemCall = wrap_vfs_method<&SQLiteVfsShim::xGetSystemCall>;
-	sqlite3_vfs::xNextSystemCall = wrap_vfs_method<&SQLiteVfsShim::xNextSystemCall>;
+	sqlite3_vfs::xOpen = wrap_vfs_method<&SQLiteVfs::xOpen>;
+	sqlite3_vfs::xDelete = wrap_vfs_method<&SQLiteVfs::xDelete>;
+	sqlite3_vfs::xAccess = wrap_vfs_method<&SQLiteVfs::xAccess>;
+	sqlite3_vfs::xFullPathname = wrap_vfs_method<&SQLiteVfs::xFullPathname>;
+	sqlite3_vfs::xDlOpen = wrap_vfs_method<&SQLiteVfs::xDlOpen>;
+	sqlite3_vfs::xDlError = wrap_vfs_method<&SQLiteVfs::xDlError>;
+	sqlite3_vfs::xDlSym = wrap_vfs_method<&SQLiteVfs::xDlSym>;
+	sqlite3_vfs::xDlClose = wrap_vfs_method<&SQLiteVfs::xDlClose>;
+	sqlite3_vfs::xRandomness = wrap_vfs_method<&SQLiteVfs::xRandomness>;
+	sqlite3_vfs::xSleep = wrap_vfs_method<&SQLiteVfs::xSleep>;
+	sqlite3_vfs::xCurrentTime = wrap_vfs_method<&SQLiteVfs::xCurrentTime>;
+	sqlite3_vfs::xGetLastError = wrap_vfs_method<&SQLiteVfs::xGetLastError>;
+	sqlite3_vfs::xCurrentTimeInt64 = wrap_vfs_method<&SQLiteVfs::xCurrentTimeInt64>;
+	sqlite3_vfs::xSetSystemCall = wrap_vfs_method<&SQLiteVfs::xSetSystemCall>;
+	sqlite3_vfs::xGetSystemCall = wrap_vfs_method<&SQLiteVfs::xGetSystemCall>;
+	sqlite3_vfs::xNextSystemCall = wrap_vfs_method<&SQLiteVfs::xNextSystemCall>;
 }
 
-int SQLiteVfsShim::register_vfs(bool makeDefault) {
+int SQLiteVfs::register_vfs(bool makeDefault) {
 	return sqlite3_vfs_register(this, makeDefault);
 }
 
-int SQLiteVfsShim::unregister_vfs() {
+int SQLiteVfs::unregister_vfs() {
 	return sqlite3_vfs_unregister(this);
 }
 
-int SQLiteVfsShim::xOpen(sqlite3_filename zName, sqlite3_file *file, int flags, int *pOutFlags) {
-	return open<SQLiteFileShim>(zName, file, flags, pOutFlags);
+int SQLiteVfs::xOpen(sqlite3_filename zName, sqlite3_file *file, int flags, int *pOutFlags) {
+	return open<SQLiteFile>(zName, file, flags, pOutFlags);
 }
-int SQLiteVfsShim::xDelete(const char *zName, int syncDir) {
+int SQLiteVfs::xDelete(const char *zName, int syncDir) {
 	return original_vfs->xDelete(original_vfs, zName, syncDir);
 }
-int SQLiteVfsShim::xAccess(const char *zName, int flags, int *pResOut) {
+int SQLiteVfs::xAccess(const char *zName, int flags, int *pResOut) {
 	return original_vfs->xAccess(original_vfs, zName, flags, pResOut);
 }
-int SQLiteVfsShim::xFullPathname(const char *zName, int nOut, char *zOut) {
+int SQLiteVfs::xFullPathname(const char *zName, int nOut, char *zOut) {
 	return original_vfs->xFullPathname(original_vfs, zName, nOut, zOut);
 }
-void *SQLiteVfsShim::xDlOpen(const char *zFilename) {
+void *SQLiteVfs::xDlOpen(const char *zFilename) {
 	return original_vfs->xDlOpen(original_vfs, zFilename);
 }
-void SQLiteVfsShim::xDlError(int nByte, char *zErrMsg) {
+void SQLiteVfs::xDlError(int nByte, char *zErrMsg) {
 	original_vfs->xDlError(original_vfs, nByte, zErrMsg);
 }
-void (*SQLiteVfsShim::xDlSym(void *library, const char *zSymbol))(void) {
+void (*SQLiteVfs::xDlSym(void *library, const char *zSymbol))(void) {
 	return original_vfs->xDlSym(original_vfs, library, zSymbol);
 }
-void SQLiteVfsShim::xDlClose(void *library) {
+void SQLiteVfs::xDlClose(void *library) {
 	return original_vfs->xDlClose(original_vfs, library);
 }
-int SQLiteVfsShim::xRandomness(int nByte, char *zOut) {
+int SQLiteVfs::xRandomness(int nByte, char *zOut) {
 	return original_vfs->xRandomness(original_vfs, nByte, zOut);
 }
-int SQLiteVfsShim::xSleep(int microseconds) {
+int SQLiteVfs::xSleep(int microseconds) {
 	return original_vfs->xSleep(original_vfs, microseconds);
 }
-int SQLiteVfsShim::xCurrentTime(double *pResOut) {
+int SQLiteVfs::xCurrentTime(double *pResOut) {
 	return original_vfs->xCurrentTime(original_vfs, pResOut);
 }
-int SQLiteVfsShim::xGetLastError(int nByte, char *zOut) {
+int SQLiteVfs::xGetLastError(int nByte, char *zOut) {
 	return original_vfs->xGetLastError(original_vfs, nByte, zOut);
 }
-int SQLiteVfsShim::xCurrentTimeInt64(sqlite3_int64 *pResOut) {
+int SQLiteVfs::xCurrentTimeInt64(sqlite3_int64 *pResOut) {
 	return original_vfs->xCurrentTimeInt64(original_vfs, pResOut);
 }
-int SQLiteVfsShim::xSetSystemCall(const char *zName, sqlite3_syscall_ptr ptr) {
+int SQLiteVfs::xSetSystemCall(const char *zName, sqlite3_syscall_ptr ptr) {
 	return original_vfs->xSetSystemCall(original_vfs, zName, ptr);
 }
-sqlite3_syscall_ptr SQLiteVfsShim::xGetSystemCall(const char *zName) {
+sqlite3_syscall_ptr SQLiteVfs::xGetSystemCall(const char *zName) {
 	return original_vfs->xGetSystemCall(original_vfs, zName);
 }
-const char *SQLiteVfsShim::xNextSystemCall(const char *zName) {
+const char *SQLiteVfs::xNextSystemCall(const char *zName) {
 	return original_vfs->xNextSystemCall(original_vfs, zName);
 }
 
