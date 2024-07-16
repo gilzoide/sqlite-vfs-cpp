e865cdce14642bf70730271b2102d456d34019d8
gilzoide <gilzoide@gmail.com>
2024-07-15 22:47:47 -0300

    Update docs

--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['iversion_0',['iVersion',['../structsqlitevfs_1_1_s_q_lite_file_impl.html#a8b8d1c564da1288a9ef35382f4f1bef8',1,'sqlitevfs::SQLiteFileImpl']]]
+  ['is_5fregistered_0',['is_registered',['../structsqlitevfs_1_1_s_q_lite_vfs.html#afae1fd2a64e83bc523dfc9abe06ce87c',1,'sqlitevfs::SQLiteVfs']]],
+  ['iversion_1',['iVersion',['../structsqlitevfs_1_1_s_q_lite_file_impl.html#a8b8d1c564da1288a9ef35382f4f1bef8',1,'sqlitevfs::SQLiteFileImpl']]]
 ];
